fb17670a6ab8cdbc452cc83c8d9d526a7c70c065
mattatz <masatatsu.nakamura@gmail.com>
2021-10-14 15:54:48 +0900

    Add webpack-node-externals in core package to reduce bundle size.

--- core/webpack.prod.js
+++ core/webpack.prod.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = merge(common, {
   mode: 'production',
+  externals: [ nodeExternals() ],
   output: {
     clean: true,
   },
